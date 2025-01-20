c5736eb6dba5820aae67c63035fa6b2926df5dc7
Shehroz <xiyobidasturchi0011@gmail.com>
2025-01-20 13:44:16 +0500

    Patch

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use Directory\app\Http\Controllers\WEB\HomeController;
-use Directory\app\Http\Controllers\WEB\UserController;
-use Directory\app\Models\User;
+use App\Http\Controllers\WEB\UserController;
+use App\Http\Controllers\WEB\HomeController;
+use App\Models\User;
 use Src\Router;
 
 Router::get('/', [HomeController::class, 'home']);
@@ -10,13 +10,7 @@ Router::get('/about', [HomeController::class, 'about']);
 Router::get('/login', [HomeController::class, 'login']);
 Router::get('/register', [HomeController::class, 'register']);
 
-Router::get('/dashboard', [UserController::class, 'home']);
-Router::get('/dashboard/quizzes', [UserController::class, 'quizzes']);
-Router::get('/dashboard/create_quiz', [UserController::class, 'create_quiz']);
-Router::get('/dashboard/statistic', [UserController::class, 'statistic']);
-Router::get('/take_quiz', [UserController::class, 'take_quiz']);
 
-Router::get('/dashboard/quizzes/{id}/update', [UserController::class, 'update']);
 
 
 Router::notFound();
